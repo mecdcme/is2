44ba144bbb0f5baa5e42b12c4445a33bfeb8178a
francescoamato <mecbox.dcme@gmail.com>
2019-06-11 11:35:45 +0200

    b

--- src/main/java/it/istat/is2/app/dao/SqlGenericDao.java
+++ src/main/java/it/istat/is2/app/dao/SqlGenericDao.java
@@ -156,7 +156,7 @@ public class SqlGenericDao {
 				+ "where  ss.dataset_file=:dFile";
 
 		if (paramsFilter != null) {
-			for (String key : paramsFilter.keySet()) {
+			for (String key : paramsFilter.keySet()) {  
 
 				query += "  and t.idx in( select f.idx  FROM   SX_DATASET_COLONNA si, json_table( CONVERT( si.daticolonna USING utf8), '$[*]' columns ( idx FOR ORDINALITY, v text path '$[0]'))f "
 						+ "  where si.dataset_file=:dFile   and si.nome=:n_" + key + " and f.v=:v_" + key + ")";
