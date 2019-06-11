74b51d47a7cf61333e6d0d23074061ab8dfbd795
iannacone <iannacone@PC82247.pc.istat.it>
2019-06-11 15:45:28 +0200

    Added attribute sxRulesClassificazioni

--- src/main/java/it/istat/is2/workflow/domain/SxRule.java
+++ src/main/java/it/istat/is2/workflow/domain/SxRule.java
@@ -68,6 +68,10 @@ public class SxRule implements Serializable {
     @ManyToOne
     @JoinColumn(name = "RULESET")
     private SxRuleset sxRuleset;
+    
+    @ManyToOne
+    @JoinColumn(name = "CLASSIFICAZIONE")
+    private SxRulesClassificazioni sxRulesClassificazioni;
 
     public SxRule() {
     }
