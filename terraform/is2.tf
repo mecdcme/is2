49f0f8b18f9d1e3acc2b0fc61e9d7e2cb3be3e5f
romaintailhurat <romain.tailhurat@gmail.com>
2020-01-14 15:02:03 +0100

    [Cloud] Change the default region for the DB instance

--- terraform/is2.tf
+++ terraform/is2.tf
@@ -1,16 +1,22 @@
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+
+
 provider "google" {
-    credentials = file("~/code/credentials/i3s-dev-b538d40535d9.json")
-    project = "i3s-dev"
-    region = "europe-west1"
-    zone = "europe-west1-a"
+  credentials = file("~/code/credentials/i3s-dev-b538d40535d9.json")
+  project     = "i3s-dev"
+  region      = var.region
+  zone        = "europe-west1-a"
 }
 
 resource "google_sql_database_instance" "master" {
-  name             = "master-instance"
+  name             = "is2-master-instance"
   database_version = "POSTGRES_11"
-  region           = "us-central1"
+  region           = var.region
 
   settings {
-      tier = "db-f1-micro"
+    tier = "db-f1-micro"
   }
 }
