663e0f3df65ee3bcb3a92ff91d6cdb25e4d5b34a
francesco amato <mecbox.dcme@gmail.com>
2020-01-31 18:38:15 +0100

    add layer selemix

--- RScripts/selemix/IS2_selemix.R
+++ RScripts/selemix/IS2_selemix.R
@@ -1,6 +1,6 @@
 # Copyright 2019 ISTAT
 # 
-#  Licensed under the EUPL, Version 1.1 or â€“ as soon they will be approved by
+#  Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
 #  the European Commission - subsequent versions of the EUPL (the "Licence");
 #  You may not use this work except in compliance with the Licence. You may
 #  obtain a copy of the Licence at:
@@ -83,7 +83,7 @@ library("rjson")
 # Impostazione manuale dataset 
 #workset <- read.csv2('C:/Users/pafrance/photon-workspace/Rscripts/input.strata.csv', dec=".", sep=";")
 
-# imposta variabili di ruolo (specifica manualmente chi Ã¨ target, covar ecc ecc)
+# imposta variabili di ruolo (specifica manualmente chi è target, covar ecc ecc)
 #Y <- "Y1"
 #X <- "X1"
 #S <- "S1"
@@ -100,8 +100,9 @@ ml_est_json = function(original_request) {
 
 
 
+
 #stima completa
-is2_mlest <- function( workset, roles, wsparams=NULL,...) {
+is2_mlest_ori <- function( workset, roles, wsparams=NULL,...) {
  	
 	stdout <- vector('character')
 	con <- textConnection('stdout', 'wr', local = TRUE)
@@ -168,6 +169,112 @@ is2_mlest <- function( workset, roles, wsparams=NULL,...) {
 	rolesgroup <- list (P= c("P", "O"),  M="M",G="G")
 	print(rolesgroup)
 	#result <-list( workset_out=r_out, roles_out=roles,rolesgroup_out=rolesgroup, params_out=mod, report=report, log=stdout)
+	result <-list( workset_out=r_out, roles_out=roles,rolesgroup_out=rolesgroup, params_out=param_mod, report_out=param_report, log=stdout)
+	
+	sink()
+	close(con)
+	return(result)
+}
+
+
+
+
+#stima completa con layer
+is2_mlest <- function( workset, roles, wsparams=NULL,...) {
+ 	
+	stdout <- vector('character')
+	con <- textConnection('stdout', 'wr', local = TRUE)
+		
+	#Default params
+	model="LN"
+	t.outl=0.5
+	lambda=3
+	w=0.05
+	lambda.fix=FALSE
+	w.fix=FALSE
+	eps=1e-7
+	max.iter=500	
+	
+	#Parameter check
+	if(!is.null(wsparams)){
+
+		if(exists("wsparams$model")) model=wsparams$model
+		if(exists("wsparams$t.outl")) t.outl=wsparams$t.outl
+		if(exists("wsparams$lambda")) lambda=wsparams$lambda
+		if(exists("wsparams$w")) w=wsparams$w
+		if(exists("wsparams$lambda.fix")) lambda.fix=wsparams$lambda.fix
+		if(exists("wsparams$w.fix")) w.fix=wsparams$w.fix
+		if(exists("wsparams$eps")) eps=wsparams$eps
+		if(exists("wsparams$max.iter")) max.iter=wsparams$max.iter
+ 	}
+	
+	# get DATASET 
+    
+	s <- workset[[roles$S]]
+	
+ 	layers <- sort(unique(s))
+
+    r_out <-c()
+    print(layers)
+	 mod <-c()
+	param_mod <-c()
+	for(layer in layers){
+		rm(workset_layer)
+		rm(x)
+		rm(y)
+		rm(s1)
+		rm(outp)
+		rm(out1)
+		 
+	    workset_layer <- workset[workset[roles$S]==layer, , drop = TRUE ]
+	  
+	    x <- workset_layer[roles$X]
+		y <- workset_layer[roles$Y]
+		s1 <- workset_layer[roles$S]
+		
+		#Execute algorithm (mettere un try catch)
+		
+		est <-  try(ml.est(y=y, x=x, model = model, lambda= as.numeric(lambda),  w= as.numeric(w), lambda.fix=lambda.fix, w.fix=w.fix, eps=as.numeric(eps), max.iter=as.numeric(max.iter), t.outl= as.numeric(t.outl), graph=FALSE))
+		predname = c()
+		out1 = c()
+		if(("est"%in%ls() & class(est)[1]!="try-error")){
+		    if(length(workset_layer)>1) ypred <- matrix(est$ypred,nrow=nrow(workset_layer),ncol=length(roles$Y))
+			else ypred <- as.matrix(est$ypred)
+			
+			#reimpostazione nomi delle variabili
+			outp <- data.frame(tau=est$tau, outlier=est$outlier, pattern=est$pattern)
+			
+			for(i in 1:ncol(ypred)) {
+				pred = ypred[,i]
+				predname = c(predname, paste("YPRED",i,sep="_"))
+				out1 <- cbind(out1,pred)
+			}
+			out1=data.frame(out1)
+			colnames(out1) <- predname
+		}
+		else{
+			 outp <- data.frame(tau=NA, outlier=NA, pattern=NA)
+		}
+			#output 
+			r_out<- rbind(r_out, cbind(x,y,s1,outp,out1))
+			
+				 
+			#output parameters
+			mod <- rbind(mod, toJSON(list(layer=layer,B=est$B, sigma=est$sigma, lambda=est$lambda, w=est$w )))
+			print(	mod)
+			#Report output 
+			report <- list(n.outlier = sum(est$outlier), missing = sum(as.numeric(est$pattern)),  is.conv = est$is.conv, sing = est$sing, bic.aic = est$bic.aic)
+			param_report <- list( Report = toJSON(report))
+		
+	} 
+  		param_mod <- list( Model = toJSON(mod))
+		print(	param_mod)
+	#setting output roles 
+	roles <- list (P= c(roles$X,roles$Y,roles$S, predname,names(outp)), O="outlier", M="Model",G="Report")
+	#setting output rolesgroup 
+	rolesgroup <- list (P= c("P", "O"),  M="M",G="G")
+		
+	#result 
 	result <-list( workset_out=r_out, roles_out=roles,rolesgroup_out=rolesgroup, params_out=param_mod, report_out=param_report, log=stdout)
 	
 	sink()
