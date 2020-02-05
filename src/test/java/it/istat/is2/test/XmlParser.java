a3655bccdfbd472cf1c823f7037f18e12a24acaa
Renzo <Renzo@LAPTOP-IC69QCM2.pc.istat.it>
2020-02-05 13:14:22 +0100

    Test inserimento primo elemento xml

--- src/test/java/it/istat/is2/test/XmlParser.java
+++ src/test/java/it/istat/is2/test/XmlParser.java
@@ -3,6 +3,7 @@ package it.istat.is2.test;
 import static org.junit.Assert.assertNull;
 
 import java.io.File;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,12 +11,23 @@ import javax.xml.bind.Unmarshaller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import it.istat.is2.app.dao.UserRolesDao;
+import it.istat.is2.workflow.dao.BusinessServiceDao;
+import it.istat.is2.workflow.domain.AppService;
+import it.istat.is2.workflow.domain.BusinessService;
 import it.istat.is2.xmlparser.domain.Service;
+import it.istat.is2.xmlparser.domain.Service.Methods;
+import it.istat.is2.xmlparser.domain.Service.Methods.Method;
+import it.istat.is2.xmlparser.domain.Service.Methods.Method.InputParameter;
+import it.istat.is2.xmlparser.domain.Service.Methods.Method.InputVariable;
+import it.istat.is2.xmlparser.domain.Service.Methods.Method.OutputVariable;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class XmlParser extends TestBase {
+	@Mock BusinessServiceDao buisiServiceDao;;
     
 	
 //	public static void main(String[] args) 
@@ -37,10 +49,63 @@ public class XmlParser extends TestBase {
             //jaxbContext = JAXBContext.newInstance("xmlparser");
             jaxbContext = JAXBContext.newInstance("it.istat.is2.xmlparser.domain");
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            // Act
+            
             Service service = (Service) jaxbUnmarshaller.unmarshal(xmlFile);
              
             System.out.println(service);
+            
+            // TEST inserimento
+            BusinessService bs=new BusinessService();
+            bs.setDescr(service.getDescription());
+            bs.setName(service.getShortname());
+            
+            // Act
+            buisiServiceDao.save(bs);
+            System.out.println("Inserimento avvenuto");
+            
+//            AppService e = new AppService();
+//            bs.getAppServices().add(e);
+            
+            Methods methods = (Methods) service.getMethods();
+            List<Method> method = (List<Method>) methods.getMethod();           	
+            	
+        	for(int y=0; y<method.size(); y++) {
+        		List<InputVariable> inputVariable = (List<InputVariable>) method.get(y).getInputVariable();
+            	
+            	if(inputVariable!=null) {
+            		for(int w=0; w<inputVariable.size(); w++) {
+            			InputVariable inputv1 = inputVariable.get(w);
+            			// Popolare domain e chiamare service per inserimento
+            			System.out.println("Popolo InputVariable");
+            		}
+            	}
+        	
+        	
+        	
+            	List<InputParameter> inputParameter = (List<InputParameter>) method.get(y).getInputParameter();
+        	
+            	if(inputParameter!=null) {            		
+            		for(int w=0; w<inputParameter.size(); w++) {
+            			InputParameter inputp1 = inputParameter.get(w);
+            			// Popolare domain e chiamare service per inserimento
+            			System.out.println("Popolo InputParameter");
+            		}
+            	}
+            	
+            	List<OutputVariable> outputVariable = (List<OutputVariable>) method.get(y).getOutputVariable();
+            	
+            	if(outputVariable!=null) {
+            		for(int w=0; w<outputVariable.size(); w++) {
+            			OutputVariable out1 = outputVariable.get(w);
+            			// Popolare domain e chiamare service per inserimento
+            			System.out.println("Popolo OutputVariable");
+            		}
+            	}            
+            	
+            	
+        	}           
+            
+            
         }
         catch (JAXBException e) 
         {
