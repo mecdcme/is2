5200b35f747903eea945ebed9d8dfbd32be7984c
Renzo <Renzo@LAPTOP-IC69QCM2.pc.istat.it>
2020-02-05 14:55:25 +0100

    bbb

--- src/test/java/it/istat/is2/test/XmlParser.java
+++ src/test/java/it/istat/is2/test/XmlParser.java
@@ -13,15 +13,12 @@ import javax.xml.bind.Unmarshaller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import it.istat.is2.app.dao.UserRolesDao;
 import it.istat.is2.workflow.dao.BusinessServiceDao;
 import it.istat.is2.workflow.domain.AppService;
 import it.istat.is2.workflow.domain.BusinessService;
-import it.istat.is2.workflow.domain.GsbpmProcess;
-import it.istat.is2.workflow.service.BusinessServiceService;
 import it.istat.is2.xmlparser.domain.Service;
 import it.istat.is2.xmlparser.domain.Service.Methods;
 import it.istat.is2.xmlparser.domain.Service.Methods.Method;
@@ -31,9 +28,7 @@ import it.istat.is2.xmlparser.domain.Service.Methods.Method.OutputVariable;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class XmlParser extends TestBase {
-	@Mock BusinessServiceDao buisiServiceDao;
-	@Mock BusinessServiceService bss;
-	
+	@Mock BusinessServiceDao buisiServiceDao;;
     
 	
 //	public static void main(String[] args) 
@@ -65,57 +60,56 @@ public class XmlParser extends TestBase {
             bs.setDescr(service.getDescription());
             bs.setName(service.getShortname());
             
-//            GsbpmProcess gsbpmProcess = new GsbpmProcess();
-//            gsbpmProcess.setId(54);
-//            bs.setGsbpmProcess(gsbpmProcess);
-            
             // Act
-            //buisiServiceDao.save(bs);
-            bss.save(bs); 
-            
+            buisiServiceDao.save(bs);
             System.out.println("Inserimento avvenuto");
             
 //            AppService e = new AppService();
 //            bs.getAppServices().add(e);
             
-            Methods methods = (Methods) service.getMethods();
-            List<Method> method = (List<Method>) methods.getMethod();           	
+            
+            List<Methods> methods = (List<Methods>) service.getMethods();
+            
+            for(int i=0; i<methods.size(); i++) {
+            	List<Method> method = methods.get(i).getMethod();
             	
-        	for(int y=0; y<method.size(); y++) {
-        		List<InputVariable> inputVariable = (List<InputVariable>) method.get(y).getInputVariable();
             	
-            	if(inputVariable!=null) {
-            		for(int w=0; w<inputVariable.size(); w++) {
-            			InputVariable inputv1 = inputVariable.get(w);
-            			// Popolare domain e chiamare service per inserimento
-            			System.out.println("Inserisco InputVariable");
-            		}
+            	for(int y=0; y<method.size(); y++) {
+            		List<InputVariable> inputVariable = (List<InputVariable>) method.get(y).getInputVariable();
+                	
+                	if(inputVariable!=null) {
+                		for(int w=0; w<inputVariable.size(); w++) {
+                			InputVariable inputv1 = inputVariable.get(w);
+                			// Popolare domain e chiamare service per inserimento
+                			System.out.println("Popolo InputVariable");
+                		}
+                	}
+            	
+            	
+            	
+                	List<InputParameter> inputParameter = (List<InputParameter>) method.get(y).getInputParameter();
+            	
+	            	if(inputParameter!=null) {            		
+	            		for(int w=0; w<inputParameter.size(); w++) {
+	            			InputParameter inputp1 = inputParameter.get(w);
+                			// Popolare domain e chiamare service per inserimento
+	            			System.out.println("Popolo InputParameter");
+                		}
+	            	}
+	            	
+	            	List<OutputVariable> outputVariable = (List<OutputVariable>) method.get(y).getOutputVariable();
+	            	
+	            	if(outputVariable!=null) {
+                		for(int w=0; w<outputVariable.size(); w++) {
+                			OutputVariable out1 = outputVariable.get(w);
+                			// Popolare domain e chiamare service per inserimento
+                			System.out.println("Popolo OutputVariable");
+                		}
+                	}            
+	            	
+	            	
             	}
-        	
-        	
-        	
-            	List<InputParameter> inputParameter = (List<InputParameter>) method.get(y).getInputParameter();
-        	
-            	if(inputParameter!=null) {            		
-            		for(int w=0; w<inputParameter.size(); w++) {
-            			InputParameter inputp1 = inputParameter.get(w);
-            			// Popolare domain e chiamare service per inserimento
-            			System.out.println("Inserisco InputParameter");
-            		}
-            	}
-            	
-            	List<OutputVariable> outputVariable = (List<OutputVariable>) method.get(y).getOutputVariable();
-            	
-            	if(outputVariable!=null) {
-            		for(int w=0; w<outputVariable.size(); w++) {
-            			OutputVariable out1 = outputVariable.get(w);
-            			// Popolare domain e chiamare service per inserimento
-            			System.out.println("Inserisco OutputVariable");
-            		}
-            	}            
-            	
-            	
-        	}           
+            }
             
             
         }
@@ -125,7 +119,7 @@ public class XmlParser extends TestBase {
         }    
         
 
-        // Assert
-        assertNotNull(fileName);       
+        // Assert       
+        assertNotNull(fileName);
     }
 }
