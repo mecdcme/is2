7db6d4b15bcb2a6770fe774af5738741553ebe23
francesco amato <mecbox.dcme@gmail.com>
2020-02-11 15:41:33 +0100

    n

--- RScripts/selemix/IS2_selemix_test.R
+++ RScripts/selemix/IS2_selemix_test.R
@@ -3,15 +3,17 @@ rm(list=ls())
 debugSource('C:/Users/framato/git/is2/RScripts/selemix/IS2_selemix.R')
 #debugSource('G:/development/git/is2/RScripts/selemix/IS2_selemix.R')
 
-worksetFile <- read.delim("G:/fatt-short.csv", header=TRUE, sep=";")
+worksetFile <- read.delim("C:/Users/framato/Downloads/datiFATT_SINTESI.csv", header=TRUE, sep=";")
 #rolesFile <- list(X="FEM_p_2016", Y="FEM_g_2017")
-rolesEst <- list(X="FEM_p_2016", Y="FEM_g_2017",S="MESE")
-rolesEdit <- list(YPRED="YPRED", Y="FEM_g_2017",S="MESE")
+rolesEst <- list(X="FEM_p_2016", Y="FEM_g_2017",S="ATE_lett")
+rolesEdit <- list(YPRED="YPRED", Y="FEM_g_2017",S="ateco")
 
 
 out <- is2_mlest (worksetFile, rolesEst)
-out1 <- is2_seledit_layer(out$workset_out, rolesEdit)
+View(out)
+#out1 <- is2_seledit_layer(out$workset_out, rolesEdit)
 View(out1)
+
 
 
 
