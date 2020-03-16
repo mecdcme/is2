c1cb4e4e6b366f1e3423302b595e1c76f10ea5a9
Renzo <Renzo@LAPTOP-IC69QCM2.home-life.hub>
2020-03-16 12:39:16 +0100

    Added mapping for newbservice and fixed some errors for business desing home.

--- src/main/java/it/istat/is2/business/design/controller/BusinessDesignController.java
+++ src/main/java/it/istat/is2/business/design/controller/BusinessDesignController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.istat.is2.app.domain.User;
 import it.istat.is2.app.service.NotificationService;
 import it.istat.is2.workflow.domain.AppService;
 import it.istat.is2.workflow.domain.BusinessService;
@@ -28,6 +34,21 @@ public class BusinessDesignController {
     public String serviceList(HttpSession session,Model model) {
         notificationService.removeAllMessages();
 
+        List<BusinessService> listaBService = businessServiceService.findBusinessServices();  
+        List<AppService> listaAppService = appServiceService.findAllAppService();
+      
+        model.addAttribute("listaBService", listaBService);
+        model.addAttribute("listaAppService", listaAppService);
+
+        return "businessdesign/home.html";
+
+    }
+    @RequestMapping(value = "/newbservice", method = RequestMethod.POST)
+    public String createNewBService(HttpSession session, Model model, @AuthenticationPrincipal User user, 
+    		@RequestParam("nome") String nome, @RequestParam("descrizione") String descrizione) {
+        notificationService.removeAllMessages();
+
+        
         List<BusinessService> listaBService = businessServiceService.findBusinessServices();  
         List<AppService> listaAppService = appServiceService.findAllAppService();
       
