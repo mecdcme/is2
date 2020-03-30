5ef48d72dfa95524cb24b842eaca01c5163d506f
Francesco Amato <francamato@gmail.com>
2020-03-30 16:00:54 +0200

    refactoring

---
+++ src/main/java/it/istat/is2/workflow/engine/EngineR.java
@@ -0,0 +1,47 @@
+package it.istat.is2.workflow.engine;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import it.istat.is2.app.service.LogService;
+import it.istat.is2.workflow.dao.AppRoleDao;
+import it.istat.is2.workflow.dao.StepRuntimeDao;
+import it.istat.is2.workflow.domain.AppRole;
+import it.istat.is2.workflow.domain.DataProcessing;
+import it.istat.is2.workflow.domain.StepInstance;
+import it.istat.is2.workflow.domain.StepRuntime;
+
+public class EngineR {
+
+
+    @Autowired
+    AppRoleDao ruoloDao;
+    @Autowired
+    StepRuntimeDao stepRuntimeDao;
+    @Autowired
+    LogService logService;
+    
+    @Value("${path.script.R}")
+    protected String pathR;
+    
+    protected String fileScriptR;
+    protected String command;
+    protected DataProcessing dataProcessing;
+    protected StepInstance stepInstance;
+    protected LinkedHashMap<String, ArrayList<StepRuntime>> dataMap;
+    protected Map<String, AppRole> rolesMap;
+    protected LinkedHashMap<String, ArrayList<String>> parametersMap;
+    protected LinkedHashMap<String, ArrayList<String>> worksetVariables;
+    protected LinkedHashMap<String, ArrayList<String>> rulesetMap;
+
+    protected LinkedHashMap<String, ArrayList<String>> variablesRolesMap;
+
+    protected LinkedHashMap<String, ArrayList<String>> worksetOut;
+    protected LinkedHashMap<String, String> parameterOut;
+    protected LinkedHashMap<String, ArrayList<String>> rolesOut;
+    protected LinkedHashMap<String, String> rolesGroupOut;
+}
