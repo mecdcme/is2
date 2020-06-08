d0d3d8331e3987a1a9a3cbbbd26d7b52d80d623d
Renzo <Renzo@LAPTOP-IC69QCM2.home-life.hub>
2020-06-08 22:29:15 +0200

    Fixed form saving

--- src/main/resources/public/js/servicedesign/wizard.js
+++ src/main/resources/public/js/servicedesign/wizard.js
@@ -24,9 +24,8 @@
 var _ctx = $("meta[name='ctx']").attr("content");
 
 $(document).ready(function() {
-	
-	
-	 // Carica le combo
+
+	// Carica le combo
 	loadParentGsbpmProcess();
 	loadBusinessServices();
 	loadAppServices();
@@ -34,102 +33,129 @@ $(document).ready(function() {
 		var sel_process = $("#select-gsbpm-1 :selected").val();
 		loadSubGsbpmProcess();
 	});
-	
-	
-	$(".next").click(function(){
-		// selservice = 0: bservice, = 1: appservice, = 2: stepinstance, = 3: end 		
+
+	$(".next").click(function() {
+		// selservice = 0: bservice, = 1: appservice, = 2: stepinstance, = 3:
+		// end
 		next_fs = $(this).parent().next();
-		var x = false;		
-		var ajaxError=false;		
-		$("#selservice").val($("fieldset").index(next_fs));		
+		var x = false;
+		var ajaxError = false;
+		$("#selservice").val($("fieldset").index(next_fs));
 		// onclick
 		execute_switch($("#selservice").val());
-		
+
 	});
-	// onload	
-	
+	// onload
+
 	showWizard();
-	
+	$("#msform").submit(function(e) {
+		e.preventDefault(); // avoid to execute the actual submit of the form.
+
+		var form = $(this);
+		var url = form.attr('action');		
+
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : form.serialize(), // serializes the form's elements.
+			success : function(data) {
+				// customize success message
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				// customize failure message
+			}
+		});
+	});
+
 });
 
-function showWizard(){
-	
-	var current_fs, next_fs, previous_fs; //fieldsets
+function showWizard() {
+
+	var current_fs, next_fs, previous_fs; // fieldsets
 	var opacity;
 
-	$(".next").click(function(){
-		
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
+	$(".next").click(
+			function() {
 
-	
+				current_fs = $(this).parent();
+				next_fs = $(this).parent().next();
 
-	// check input errrors before proceed
-	var inputError = $("#inputerror").val();
-	if(inputError!="1"){
-		//alert(inputError);
-		//Add Class Active
-		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-		//show the next fieldset
-		next_fs.show();
-		//hide the current fieldset with style
-		current_fs.animate({opacity: 0}, {
-		step: function(now) {
-		// for making fielset appear animation
-		opacity = 1 - now;
+				// check input errrors before proceed
+				var inputError = $("#inputerror").val();
+				if (inputError != "1") {
+					// alert(inputError);
+					// Add Class Active
+					$("#progressbar li").eq($("fieldset").index(next_fs))
+							.addClass("active");
+					// show the next fieldset
+					next_fs.show();
+					// hide the current fieldset with style
+					current_fs.animate({
+						opacity : 0
+					}, {
+						step : function(now) {
+							// for making fielset appear animation
+							opacity = 1 - now;
 
-		current_fs.css({
-		'display': 'none',
-		'position': 'relative'
-		});
-		next_fs.css({'opacity': opacity});
-		},
-		duration: 600
-		});
-		
-		
-	}else{
-		//alert(inputError);
-	}
+							current_fs.css({
+								'display' : 'none',
+								'position' : 'relative'
+							});
+							next_fs.css({
+								'opacity' : opacity
+							});
+						},
+						duration : 600
+					});
+
+				} else {
+					// alert(inputError);
+				}
+			});
+
+	$(".previous").click(
+			function() {
+
+				current_fs = $(this).parent();
+				previous_fs = $(this).parent().prev();
+
+				// Remove class active
+				$("#progressbar li").eq($("fieldset").index(current_fs))
+						.removeClass("active");
+
+				// show the previous fieldset
+				previous_fs.show();
+
+				// hide the current fieldset with style
+				current_fs.animate({
+					opacity : 0
+				}, {
+					step : function(now) {
+						// for making fielset appear animation
+						opacity = 1 - now;
+
+						current_fs.css({
+							'display' : 'none',
+							'position' : 'relative'
+						});
+						previous_fs.css({
+							'opacity' : opacity
+						});
+					},
+					duration : 600
+				});
+			});
+
+	$('.radio-group .radio').click(function() {
+		$(this).parent().find('.radio').removeClass('selected');
+		$(this).addClass('selected');
 	});
-	
-	$(".previous").click(function(){
 
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-
-	//Remove class active
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-	//show the previous fieldset
-	previous_fs.show();
-
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-	step: function(now) {
-	// for making fielset appear animation
-	opacity = 1 - now;
-
-	current_fs.css({
-	'display': 'none',
-	'position': 'relative'
-	});
-	previous_fs.css({'opacity': opacity});
-	},
-	duration: 600
-	});
-	});
-
-	$('.radio-group .radio').click(function(){
-	$(this).parent().find('.radio').removeClass('selected');
-	$(this).addClass('selected');
-	});
-
-	$(".submit").click(function(){
-	return false;
+	$(".submit").click(function() {
+		return false;
 	})
 }
-function loadParentGsbpmProcess() {	
+function loadParentGsbpmProcess() {
 	$
 			.ajax({
 				url : _ctx + "/loadGsbpmParentProcess",
@@ -197,7 +223,7 @@ function loadSubGsbpmProcess() {
 				}
 			});
 }
-function loadBusinessServices() {	
+function loadBusinessServices() {
 	$
 			.ajax({
 				url : _ctx + "/loadbusinessservices",
@@ -222,7 +248,7 @@ function loadBusinessServices() {
 							});
 
 					content += "</select><span class='help-block'></span></div>";
-					$("#select-bservice-div").html(content);					
+					$("#select-bservice-div").html(content);
 
 				},
 				error : function(jqXHR, textStatus, errorThrown) {
@@ -230,7 +256,7 @@ function loadBusinessServices() {
 				}
 			});
 }
-function loadAppServices() {	
+function loadAppServices() {
 	$
 			.ajax({
 				url : _ctx + "/loadapplicationservices",
@@ -255,7 +281,7 @@ function loadAppServices() {
 							});
 
 					content += "</select><span class='help-block'></span></div>";
-					$("#select-appservice-div").html(content);					
+					$("#select-appservice-div").html(content);
 
 				},
 				error : function(jqXHR, textStatus, errorThrown) {
@@ -263,45 +289,27 @@ function loadAppServices() {
 				}
 			});
 }
-function saveAllData(){
-	$("#msform").submit(function(e) {
-
-	    e.preventDefault(); // avoid to execute the actual submit of the form.
-
-	    var form = $(this);
-	    var url = form.attr('action');
-
-	    $.ajax({
-	           type: "POST",
-	           url: url,
-	           data: form.serialize(), // serializes the form's elements.
-	           success: function(data)
-	           {
-	               alert(data); // show response from the php script.
-	           }
-	         });
-
-
-	});
+function saveAllData() {
+	
 }
 
-function validate_bs_and_send_req() {		
+function validate_bs_and_send_req() {
 	var bsname = $('#bs-name').val();
 	if (bsname.length < 1) {
 		$("#bs-name-error").text(_mandatory_name_field);
 		$("#inputerror").val(1);
-	} else {		
+	} else {
 		$("#inputerror").val(0);
-		//$("#bs-form").submit();
+		// $("#bs-form").submit();
 	}
 }
-function validate_as_and_send_req() {	
+function validate_as_and_send_req() {
 	var asname = $('#as-name').val();
 	if (asname.length < 1) {
 		$("#as-name-error").text(_mandatory_name_field);
 		$("#inputerror").val(1);
 	} else {
-		//$("#as-form").submit();
+		// $("#as-form").submit();
 		$("#inputerror").val(0);
 	}
 }
@@ -309,38 +317,38 @@ function validate_si_and_send_req() {
 	var method = $('#si-method').val();
 	if (method.length < 1) {
 		$("#si-method-error").text(_mandatory_method_field);
-		$("#inputerror").val(1);		
+		$("#inputerror").val(1);
 	} else {
 		$("#msform").submit();
 		$("#inputerror").val(0);
-		//saveAllData();
+		// saveAllData();
 	}
 }
-function execute_switch(stepIndex){	
-	switch (stepIndex*1) {
-	
-		case 0:
-			
-			//alert("tab 1");
-		
+function execute_switch(stepIndex) {
+	switch (stepIndex * 1) {
+
+	case 0:
+
+		// alert("tab 1");
+
 		break;
-		case 1:
-			//alert($("#selservice").val());
-			// velidate fields bservice
-			validate_bs_and_send_req();
-			//alert("tab 2");
-		
+	case 1:
+		// alert($("#selservice").val());
+		// velidate fields bservice
+		validate_bs_and_send_req();
+		// alert("tab 2");
+
 		break;
-		case 2:
-			//alert($("#selservice").val());
-			validate_as_and_send_req();
-			//alert("tab 3");
-		
+	case 2:
+		// alert($("#selservice").val());
+		validate_as_and_send_req();
+		// alert("tab 3");
+
 		break;
-		case 3:
-			//alert($("#selservice").val());
-			validate_si_and_send_req();			
-		
+	case 3:
+		// alert($("#selservice").val());
+		validate_si_and_send_req();
+
 		break;
 	}
 }
