01872891f21a5519fe6e80e26b0e92c81188b742
Francesco Amato <francamato@gmail.com>
2020-07-07 12:23:55 +0200

    fix

--- src/main/java/it/istat/is2/app/util/IS2Exception.java
+++ src/main/java/it/istat/is2/app/util/IS2Exception.java
@@ -1,12 +1,15 @@
 package it.istat.is2.app.util;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class IS2Exception extends Exception {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 485598706504813112L;
-	IS2ExceptionCodes code;
+	private IS2ExceptionCodes code;
 
 	public IS2Exception(IS2ExceptionCodes code) {
 		super();
