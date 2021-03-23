a200aa9e2869a6d7269dcb718ab461b57e841bf4
Pierre <Pierre Le Corre>
2021-03-23 10:24:38 +0100

    Add a separate Keycloak authentication configuration
    The app use either the default IS2 configuration or Keycloak.
    
    The decision is automatically made by looking for the "keycloak.realm" property.
    If it is absent, the default security configuration is used.
    If it is present, Keycloak is used.

---
+++ src/main/java/it/istat/is2/KeycloakSecurityConfig.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2019 ISTAT
+ * <p>
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence. You may
+ * obtain a copy of the Licence at:
+ * <p>
+ * http://ec.europa.eu/idabc/eupl5
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * Licence for the specific language governing permissions and limitations under
+ * the Licence.
+ *
+ */
+package it.istat.is2;
+
+import org.keycloak.adapters.springboot.KeycloakAutoConfiguration;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@ConditionalOnProperty("keycloak.realm")
+@EnableWebSecurity
+@Configuration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Configuration
+    public static class KeycloakConfig extends KeycloakAutoConfiguration {
+        // Force Keycloak autoconfiguration (disabled by default In IS2Application)
+    }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+            .authorizeRequests()
+            .antMatchers("/libs/**", "/js/**", "/img/**", "/css/**", "/icons/**", "/fonts/**").permitAll()
+            .anyRequest().authenticated()
+            .and().logout().logoutSuccessUrl("/sso/login");
+    }
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+    
+    @Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(keycloakAuthenticationProvider());
+	}
+    
+     /** Tells Keycloak to use application.properties instead of keycloak.json to find its config. */
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+}
