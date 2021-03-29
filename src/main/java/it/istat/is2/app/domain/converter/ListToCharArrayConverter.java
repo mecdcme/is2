96eb34d61223bf3b4765dcd2e2e9d607ef7baf7b
Francesco Amato <francamato@gmail.com>
2021-03-29 19:03:36 +0200

    fix db settings

---
+++ src/main/java/it/istat/is2/app/domain/converter/ListToCharArrayConverter.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2019 ISTAT
+ * <p>
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence. You may
+ * obtain a copy of the Licence at:
+ * <p>
+ * http://ec.europa.eu/idabc/eupl5
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * Licence for the specific language governing permissions and limitations under
+ * the Licence.
+ *
+ * @author Francesco Amato <framato @ istat.it>
+ * @author Mauro Bruno <mbruno @ istat.it>
+ * @author Paolo Francescangeli  <pafrance @ istat.it>
+ * @author Renzo Iannacone <iannacone @ istat.it>
+ * @author Stefano Macone <macone @ istat.it>
+ * @version 1.0
+ */
+package it.istat.is2.app.domain.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+@Converter
+public class ListToCharArrayConverter implements AttributeConverter<List<char[]>, String> {
+    final static Logger logger = Logger.getLogger(ListToCharArrayConverter.class);
+
+    @Override
+    public String convertToDatabaseColumn(List<char[]> data) {
+        String value = null;
+
+        if (data != null) {
+            JSONArray allDataArray = new JSONArray();
+
+            for (int index = 0; index < data.size(); index++) {
+
+                allDataArray.put(data.get(index) != null ? String.valueOf(data.get(index)) : "");
+            }
+
+            value = allDataArray.toString();
+        }
+
+        return value;
+    }
+
+    @Override
+    public List<char[]> convertToEntityAttribute(String data) {
+        List<char[]> listValue = new ArrayList<char[]>();
+        try {
+            if (data != null) {
+                JSONArray jsonArray = new JSONArray(data);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    listValue.add(jsonArray.get(i).toString().toCharArray());
+                }
+            }
+        } catch (JSONException e) {
+
+            logger.error(e);
+            logger.error(data);
+        }
+        return listValue;
+    }
+
+}
